0b5a9cf44252aadbb6ba23b4ad53f8f7375c3e6f
tempeight0008 <tempeight0008@gmail.com>
2026-06-16 09:52:01 +0000

    Auth setup: added signup action

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -18,3 +18,26 @@ export async function login(formData: FormData) {
     revalidatePath("/", "layout");
     return redirect("/dashboard");
 }
+
+export async function signup(formData: FormData){
+    const supabase = await createClient();
+
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const fullName = formData.get('fullName') as string
+    const role = formData.get('role') as string
+
+    const {error} = await supabase.auth.signUp({email, password, options: {
+        data: {
+            role,
+            full_name: fullName
+        }
+    }})
+
+    if (error) {
+    return redirect(`/login?error=${encodeURIComponent(error.message)}`)
+    }
+
+    revalidatePath('/', 'layout')
+    return redirect('/dashboard')
+}
